bd051f6dfcaa9418a3e8afe1f2c38e08919a4ec6
Satharus <a.elmayyah@gmail.com>
2021-04-25 00:01:06 +0200

    Added Ceasar Cipher.

--- securitylibrary/MainAlgorithms/Ceaser.cs
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -10,18 +10,44 @@ namespace SecurityLibrary
     {
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
-            
+            StringBuilder encryptedText = new StringBuilder(plainText);
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                encryptedText[i] += (char)key;
+                if (!(encryptedText[i] >= 'A' && encryptedText[i] <= 'Z') &&
+                    !(encryptedText[i] >= 'a' && encryptedText[i] <= 'z'))
+                {
+                    encryptedText[i] -= (char)26;
+                }
+            }
+            return encryptedText.ToString();
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            StringBuilder decryptedText = new StringBuilder(cipherText);
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                decryptedText[i] -= (char)key;
+                if (!(decryptedText[i] >= 'A' && decryptedText[i] <= 'Z') &&
+                    !(decryptedText[i] >= 'a' && decryptedText[i] <= 'z'))
+                {
+                    decryptedText[i] += (char)26;
+                }
+            }
+            return decryptedText.ToString();
         }
 
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            int key = (int)cipherText[0] - plainText[0];
+
+            if (key >= 26) key -= 26;
+            else if (key < 0) key += 26;
+
+            return Math.Abs(key);
         }
     }
 }
