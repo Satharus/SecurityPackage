6d6892330aa3a69ebf4be2fab02b2bfe97cd2d2e
Satharus <a.elmayyah@gmail.com>
2021-05-07 02:00:55 +0200

    Added Vigenere code, the analysis works but I can't find a way to determine the key length to return just the key and not the whole keystream.

--- securitylibrary/MainAlgorithms/AutokeyVigenere.cs
+++ securitylibrary/MainAlgorithms/AutokeyVigenere.cs
@@ -8,19 +8,102 @@ namespace SecurityLibrary
 {
     public class AutokeyVigenere : ICryptographicTechnique<string, string>
     {
+        char[,] alphabetMatrix = new char[26, 26];
+
+        public AutokeyVigenere()
+        {
+            char start = 'A';
+            for (int i = 0; i < 26; i++)
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    if ((start + j) > 'Z')
+                    {
+                        alphabetMatrix[i, j] = (char)((start + j) - 'Z' + 'A' - 1);
+                    }
+                    else
+                    {
+                        alphabetMatrix[i, j] = (char)(start + j);
+                    }
+                }
+                start++;
+            }
+        }
+
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            StringBuilder keyStream = new StringBuilder();
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                char keyChar = '.';
+                for (int j = 0; j < 26; j++)
+                {
+                    if (cipherText[i] == this.alphabetMatrix[j, plainText[i] - 'a'])
+                    {
+                        keyChar = (char)('a' + j);
+                    }
+                }
+                keyStream.Append(keyChar);
+            }
+
+            return keyStream.ToString().ToLower();
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            StringBuilder keyStream = new StringBuilder();
+            StringBuilder plainText = new StringBuilder();
+            keyStream.Append(key);
+            int k = 0;
+            while (keyStream.Length < cipherText.Length)
+            {
+
+                char keystreamChar = '.';
+                for (int j = 0; j < 26; j++)
+                {
+                    if (cipherText[k] == this.alphabetMatrix[keyStream[k] - 'a', j])
+                    {
+                        keystreamChar = (char)('a' + j);
+                    }
+                }
+                keyStream.Append(keystreamChar);
+                k++;
+            }
+
+            
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                char plainChar = '.';
+                for (int j = 0; j < 26; j++)
+                {
+                    if (cipherText[i] == this.alphabetMatrix[keyStream[i] - 'a', j])
+                    {
+                        plainChar = (char)('a' + j);
+                    }
+                }
+                plainText.Append(plainChar);
+            }
+
+            return plainText.ToString().ToLower();
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            StringBuilder keyStream = new StringBuilder();
+            StringBuilder cipherText = new StringBuilder();
+            keyStream.Append(key);
+            while (keyStream.Length < plainText.Length)
+            {
+                keyStream.Append(plainText);
+            }
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                cipherText.Append(this.alphabetMatrix[plainText[i] - 'a', keyStream[i] - 'a']);
+            }
+
+            return cipherText.ToString().ToUpper();
         }
     }
 }
--- securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -8,19 +8,87 @@ namespace SecurityLibrary
 {
     public class RepeatingkeyVigenere : ICryptographicTechnique<string, string>
     {
+        char[,] alphabetMatrix = new char[26, 26];
+
+        public RepeatingkeyVigenere()
+        {
+            char start = 'A';
+            for (int i = 0; i < 26; i++)
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    if ((start + j) > 'Z')
+                    {
+                        alphabetMatrix[i, j] = (char)((start + j)-'Z' + 'A' - 1);
+                    }
+                    else
+                    {
+                        alphabetMatrix[i, j] = (char)(start + j);
+                    }
+                }
+                start++;
+            }
+        }
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            StringBuilder keyStream = new StringBuilder();
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                char keyChar = '.';
+                for (int j = 0; j < 26; j++)
+                {
+                    if (cipherText[i] == this.alphabetMatrix[j, plainText[i] - 'a'])
+                    {
+                        keyChar = (char)('a' + j);
+                    }
+                }
+                keyStream.Append(keyChar);
+            }
+
+            return keyStream.ToString().ToLower();
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            StringBuilder keyStream = new StringBuilder();
+            StringBuilder plainText = new StringBuilder();
+            while (keyStream.Length < cipherText.Length)
+            {
+                keyStream.Append(key);
+            }
+
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                char plainChar = '.';
+                for (int j = 0; j < 26; j++)
+                {
+                    if (cipherText[i] == this.alphabetMatrix[keyStream[i] - 'a', j])
+                    {
+                        plainChar = (char)('a' + j);
+                    }
+                }
+                plainText.Append(plainChar);
+            }
+
+            return plainText.ToString().ToLower();
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            StringBuilder keyStream = new StringBuilder();
+            StringBuilder cipherText = new StringBuilder();
+            while (keyStream.Length < plainText.Length)
+            {
+                keyStream.Append(key);
+            }
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                cipherText.Append(this.alphabetMatrix[plainText[i] - 'a', keyStream[i] - 'a']);    
+            }
+
+            return cipherText.ToString().ToUpper();
         }
     }
 }
