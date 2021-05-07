17239d78d09b1f8c525358d51b9d70a512f02178
Danyaaa <dania.r.hamouda@gmail.com>
2021-05-07 22:24:07 +0200

    RailFence Algorithm

--- securitylibrary/MainAlgorithms/RailFence.cs
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -10,17 +10,69 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            string ret;
+            int key = 0;
+            for (int i = 1; i < plainText.Length; i++)
+            {
+                ret = Encrypt(plainText, i);
+                if (ret == cipherText)
+                {
+                    key = i; break;
+                }
+            }
+            return key;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+
+            float keyy = (float)key;
+            float val = cipherText.Length / keyy;
+            double plaintext_len = Math.Ceiling(val);
+            int len = Convert.ToInt32(plaintext_len);
+            string ret = "";
+            List<string> plain = new List<string>();
+            int count = 0;
+            for (int i = 0; i < len; i++)
+            {
+                count = i;
+                for (int j = 0; j < key; j++)
+                {
+                    if (count < cipherText.Length)
+                    {
+                        ret += cipherText[count];
+                        count += len;
+                    }
+
+                }
+
+            }
+            return ret.ToLower();
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            List<string> ciphertext = new List<string>();
+
+            int j = 0;
+            int cont = 0;
+            for (int i = 0; i < key; i++)
+            {
+                string val = "";
+                j = cont;
+                for (; j < plainText.Length; j += key)
+                {
+                    val += plainText[j];
+                }
+                ciphertext.Add(val);
+                cont++;
+
+            }
+            string ret = "";
+            for (int i = 0; i < key; i++)
+                ret += ciphertext[i];
+            return ret.ToUpper();
+
         }
     }
 }
