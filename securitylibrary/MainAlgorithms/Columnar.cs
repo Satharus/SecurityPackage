76c83d1633bd04fdc55c462720378a9a620b2e7a
Satharus <a.elmayyah@gmail.com>
2021-05-08 02:55:33 +0200

    Added columnar decryption

--- securitylibrary/MainAlgorithms/Columnar.cs
+++ securitylibrary/MainAlgorithms/Columnar.cs
@@ -15,7 +15,56 @@ namespace SecurityLibrary
 
         public string Decrypt(string cipherText, List<int> key)
         {
-            throw new NotImplementedException();
+            int columns = key.Count;
+            int rows = cipherText.Length / columns;
+
+            if (cipherText.Length % columns != 0) rows++;
+
+            char[,] tempCipher = new char[rows, columns];
+
+            StringBuilder[] partitionedCipherText = new StringBuilder[columns];
+
+            int j = 0;
+            for (int i = 0; i < cipherText.Length; i+=rows)
+            {
+                partitionedCipherText[j] = new StringBuilder();
+                if (i + rows < cipherText.Length)
+                {
+                    partitionedCipherText[j].Append(cipherText.Substring(i, rows));
+                    j++;
+                }
+                else
+                {
+                    partitionedCipherText[j].Append(cipherText.Substring(i));
+                    j++;
+                }
+            }
+
+            Dictionary<int, int> swaps = new Dictionary<int, int>();
+            for (int i = 0; i < key.Count; i++)
+            {
+                swaps.Add(key[i] - 1, i);
+            }
+
+            StringBuilder[] sortedCipherText = new StringBuilder[columns];
+            foreach (var swap in swaps)
+            {
+                sortedCipherText[swap.Value] = partitionedCipherText[swap.Key];
+            }
+
+            StringBuilder plainText = new StringBuilder();
+            for(int i = 0; i < rows; i++)
+            {
+                for (int k = 0; k < columns; k++)
+                {
+                    if (i < sortedCipherText[k].Length)
+                    {
+                        plainText.Append(sortedCipherText[k][i]);
+                    }
+                }
+            }
+
+            return plainText.ToString();
         }
 
         public string Encrypt(string plainText, List<int> key)
